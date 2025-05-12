13353637fb7e6ba8345f66f82c340d9c4f93b274
Dong Yan <dong.yan@yara.com>
2025-05-12 22:20:19 +0200

    add code

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice';
+
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
@@ -254,8 +256,13 @@ function ProductList({ onHomeClick }) {
         setShowCart(false);
     };
 
-    const handleAddToCart = (e) => {
+    const handleAddToCart = (product) => {
         console.log("Adding to cart");
+        dispatch(addItem(product))
+        setAddedToCart((prevState) => ({
+            ...prevState,
+            [product.name]: true,
+        }));
     };
 
     return (
