bed7673f2b92b9ec2d20d8d783e1a0d00ab81fc9
Dong Yan <dong.yan@yara.com>
2025-05-14 15:01:45 +0200

    add code

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -9,7 +9,7 @@ export const CartSlice = createSlice({
     addItem: (state, action) => {
       const { name, image, cost } = action.payload;
       const existingItem = state.items.find(item => item.name === name);
-      if(existingItem) {
+      if (existingItem) {
         existingItem.quantity++;
       } else {
         state.items.push({ name, image, cost, quantity: 1 });
